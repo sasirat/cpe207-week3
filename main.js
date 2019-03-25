733ed5cfb5aaf369d3e0593745b4033247bb3a32
sasirat <sasirat019@gmail.com>
2019-03-25 15:27:40 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log("Hello man");
+alert("this is alert");
+
+let age = 30;
+age = 31;
+console.log("age : ",age);
+console.log("age : "+30);
+console.log(30+" cm");
+age = '45';
+console.log(age);
+console.log(30 + '40');
+let b = 40 +'30';
+console.log(b);
